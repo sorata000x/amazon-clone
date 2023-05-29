52b0a34c64e21daee5f3c10dd76ddac3f98ee8a4
Sora <70079892+sorata000x@users.noreply.github.com>
2023-05-29 12:43:34 -0700

    Completion of Part3.5: More Checkout Page

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,9 @@ export const initialState = {
     basket: [],
 };
 
+export const getBasketTotal = (basket) => 
+	basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
 	console.log(action);
 	switch (action.type) {
@@ -10,6 +13,25 @@ const reducer = (state, action) => {
 				...state,
 				basket: [...state.basket, action.item],
 			};
+		case "REMOVE_FROM_BASKET":
+			const index = state.basket.findIndex(
+				(basketItem) => basketItem.id === action.id
+			);
+			let newBasket = [...state.basket];
+
+			if (index >= 0) {		// remove item from index
+				newBasket.splice(index, 1);
+			} else {
+				console.warn(
+					`Cant remove product (id: ${action.id}) as its not in the basket!`
+				)
+			}
+
+			return {
+				...state, 
+				basket: newBasket
+			}
+
 		default:
 			return state;
 	}
